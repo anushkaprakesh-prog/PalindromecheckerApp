367f134f104eb32086846d572f0584f637d0f478
anushkaprakesh-prog <anushkaprakesh@gmail.com>
2026-03-17 15:33:43 +0530

    Print a Hardcoded Palindrome Result

--- PalindromecheckerApp/src/PalindromecheckerApp.java
+++ PalindromecheckerApp/src/PalindromecheckerApp.java
@@ -1,6 +1,20 @@
 public class PalindromecheckerApp {
-    static void main() {
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version 1.0");
+    public static void main(String[] args) {
+
+        // Hardcoded string (String Literal)
+        String original = "madam";
+
+        // Reverse the string
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
     }
 }
